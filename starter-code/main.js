b51760669fa14625d797a3389c0476444e5d19b7
Kevin Kuo <Kevin Kuo>
2017-01-24 22:20:41 -0800

    added conditional

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,19 @@
+console.log()
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardTwo) {
+	window.alert("You found a match!");
+} else if (cardThree === cardFour) {
+	window.alert("You found a match!");
+} else if (cardOne === cardThree) {
+	window.alert("Sorry, try again.");
+} else if (cardOne === cardFour) {
+	window.alert("Sorry, try again.");
+} else if (cardTwo === cardThree) {
+	window.alert("Sorry, try again.");
+} else if (cardTwo === cardFour) {
+	window.alert("Sorry, try again.");
+}
